b482a7691517b34839d5d1c513493dc6091e93b1
jiangweiye <jiangwy@staryea.com>
2024-07-31 13:50:14 +0800

    增加kv错误检查

--- _worker.js
+++ _worker.js
@@ -5784,7 +5784,17 @@ var src_default = {
         const host = url.origin;
         const frontendUrl =
             env.FRONTEND || `https://raw.githubusercontent.com/jwyGithub/subconverter-cloudflare/main/index.html?t=${Date.now()}`;
+
+
+        if(env.SUB_BUCKET === undefined) {
+            return new Response('环境变量SUB_BUCKET未设置', { status: 400 });
+        }
+
         const SUB_BUCKET = env[env.SUB_BUCKET];
+        if(SUB_BUCKET === undefined) {
+            return new Response(`kv绑定错误，env SUB_BUCKET is ${env.SUB_BUCKET}`, { status: 400 });
+        }
+
         const REMOTE_CONFIG = env.REMOTE_CONFIG || '';
         const LOCK_CONFIG = env.LOCK_BACKEND || 'true';
         const remoteConfig = REMOTE_CONFIG === '' ? [] : getRemoteConfig(REMOTE_CONFIG);
